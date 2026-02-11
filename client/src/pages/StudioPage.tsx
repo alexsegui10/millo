3579791b70efb533751a0daa1e779451ee6c1957
alexsegui10 <alexsegui10@gmail.com>
2026-02-11 23:06:56 +0100

    fix: Remove unused Task import to satisfy linter

--- client/src/pages/StudioPage.tsx
+++ client/src/pages/StudioPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { listTasks, createTask, toggleTask, deleteTask } from '../lib/apiClient';
-import { Task } from '../types';
 import { useNavigate } from 'react-router-dom';
 
 export function StudioPage() {
